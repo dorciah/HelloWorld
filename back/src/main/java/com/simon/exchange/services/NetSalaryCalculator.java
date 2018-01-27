3f086d1c4acbafbc5fed6f615da5121e9b28bb66
sphall <simonhall37@hotmail.com>
2018-01-27 20:52:19 +0000

    Implemented methods in service classes

--- back/src/main/java/com/simon/exchange/services/NetSalaryCalculator.java
+++ back/src/main/java/com/simon/exchange/services/NetSalaryCalculator.java
@@ -1,10 +1,18 @@
 package com.simon.exchange.services;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.simon.exchange.domain.ClientRequest;
 import com.simon.exchange.domain.ClientResponse;
 import com.simon.exchange.domain.Country;
+import com.simon.exchange.domain.Rate;
 import com.simon.exchange.domain.RateResponse;
 import com.simon.exchange.domain.Result;
 
@@ -16,26 +24,112 @@ import com.simon.exchange.domain.Result;
  *
  */
 
+@Service
 public class NetSalaryCalculator {
 
+	private final Logger logger = Logger.getLogger(NetSalaryCalculator.class);
+	private final String HOME_COUNTRY = "Poland";
+	private final int DAYS = 22;
+	
+	@Autowired
+	CountryRepository repository;
+	
+	@Autowired
+	private RateService rateService;
+	
 	public NetSalaryCalculator() {}
 	
+	public CountryRepository getRepository() {
+		return this.repository;
+	}
+	
 	public ClientResponse calculate(ClientRequest request) {
-		return null;
+		
+		final ClientResponse response = new ClientResponse();
+		
+		// first get the country
+		Country country = getCountry(request.getCountry());
+		if (country == null) throw new IllegalArgumentException("Invalid Country name in request");
+		else if (request.getAmount() < 0) throw new IllegalArgumentException("Daily salary is negative!");
+		else {
+			
+			// get the rate if not home country
+			RateResponse RR = null;
+			Rate R = null;
+			if (!country.getName().equalsIgnoreCase(this.HOME_COUNTRY)) {
+				RR = getRate(country.getCurrency());
+				if (RR == null) {
+					throw new NullPointerException("Exchange rate api did not return a response");
+				}
+				R = RR.getRates().get(0);
+				response.setEffectiveDate(R.getEffectiveDate());
+				response.setRate(R.getAsk());
+			}
+			
+			// calculate the results
+			List<Result> results = generateResults(request, RR, country);
+			
+			response.setRequest(request);
+			response.setResults(results);
+			
+			return response;
+		}
 	}
 	
 	public Country getCountry(String name) {
-		return null;
+		return repository.getCountryByName(name);
 	}
 	
 	public RateResponse getRate(String currency) {
-		return null;
+		try {
+			return this.rateService.getRateResponse(currency);
+		} catch (NullPointerException e) {
+			logger.error("Unable to get rate for currency " + currency);
+			return null;
+		}
 	}
 	
 	public List<Result> generateResults(ClientRequest request, RateResponse RR, Country country){
-		return null;
+		
+		final List<Result> results = new ArrayList<>();
+		final double monthlyGross = this.DAYS * request.getAmount();
+		
+		Result gross = new Result(0,"gross",round(monthlyGross,2),0d);
+		logger.info(gross);
+		results.add(gross);
+		
+		// first do the tax
+		Result postTax = new Result(1,"post-tax",
+				round(monthlyGross*(1-country.getTax()),2), round(monthlyGross -monthlyGross*(1-country.getTax()),2));
+		logger.info(postTax);
+		results.add(postTax);
+		
+		// deduct costs
+		Result postDeduction = new Result(2,"post-deduction",round(postTax.getAmount() - country.getDeductions(),2),country.getDeductions());
+		logger.info(postDeduction);
+		results.add(postDeduction);
+		
+		// if not base country then use exchange rate
+		if (!this.HOME_COUNTRY.equalsIgnoreCase(country.getName())) {
+			final Rate rate = RR.getRates().get(0);
+			Result postExchange = 
+					new Result(3,"post-exchange",round(postDeduction.getAmount()*rate.getAsk(),2)
+							,0d);
+			logger.info(postExchange);
+			results.add(postExchange);
+		}
+		
+		return results;
 	}
-	
+
+	public static double round(double value, int places) {
+	    
+		if (places < 0) throw new IllegalArgumentException();
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	    
+	}
 	
 	
 }
--- back/src/main/java/com/simon/exchange/services/RateService.java
+++ back/src/main/java/com/simon/exchange/services/RateService.java
@@ -1,6 +1,7 @@
 package com.simon.exchange.services;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,10 +15,11 @@ import com.simon.exchange.domain.RateResponse;
  *
  */
 
+@Service
 public class RateService {
 
 	private final Logger logger = Logger.getLogger(RateService.class);
-	private final String url = "http://api.nbp.pl/api/exchangerates/rates/C/aaaa";
+	private final String url = "http://api.nbp.pl/api/exchangerates/rates/C/";
 	private final String params = "?format=json";
 	private RestTemplate restTemplate;
 	
@@ -25,12 +27,12 @@ public class RateService {
 		this.restTemplate = new RestTemplate();
 	}
 	
-	public RateResponse getRateResponse(String currency) {
-
+	public RateResponse getRateResponse(String currency) throws NullPointerException {
+		final String targetAddress = getUrl() + currency + getParams();
 		try{
-			return this.restTemplate.getForObject(getUrl() + currency + getParams(), RateResponse.class);
+			return this.restTemplate.getForObject(targetAddress, RateResponse.class);
 		} catch (RestClientException ex) {
-			logger.error("Could not get a rate for " + currency + " - " + ex.getMessage() ,ex);
+			logger.error("Could not get a rate for " + currency + " from " + targetAddress +" - " + ex.getMessage() ,ex);
 			throw new NullPointerException("Exchange rate api did not return a response");
 		}
 	}
