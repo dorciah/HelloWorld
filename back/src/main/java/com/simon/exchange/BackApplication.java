512b653db412fbaf66606fd60161ac2bee37c2f5
sphall <simonhall37@hotmail.com>
2018-01-27 20:50:47 +0000

    Added 3 countries to startup

--- back/src/main/java/com/simon/exchange/BackApplication.java
+++ back/src/main/java/com/simon/exchange/BackApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.simon.exchange.domain.Country;
 import com.simon.exchange.services.CountryRepository;
 
 /**
@@ -25,7 +26,9 @@ public class BackApplication {
 	@Bean
 	public CommandLineRunner demo(CountryRepository repository) {
 		return (args) -> {
-			
+			repository.save(new Country(1L,"UK","GBP",0.25d,600.00d));
+			repository.save(new Country(2L,"Germany","EUR",0.20d,800.00d));
+			repository.save(new Country(3L,"Poland","PLN",0.19d,1200.00d));
 		};
 	}
 }
