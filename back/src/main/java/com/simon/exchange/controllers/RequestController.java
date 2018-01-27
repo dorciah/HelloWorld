475091b8b1560fd2232b5b1eb38a0fe4c8df282e
sphall <simonhall37@hotmail.com>
2018-01-27 20:53:05 +0000

    Implemented controller and test cases

--- back/src/main/java/com/simon/exchange/controllers/RequestController.java
+++ back/src/main/java/com/simon/exchange/controllers/RequestController.java
@@ -3,7 +3,9 @@ package com.simon.exchange.controllers;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.simon.exchange.domain.ClientRequest;
 import com.simon.exchange.domain.ClientResponse;
 import com.simon.exchange.domain.Country;
+import com.simon.exchange.services.CountryRepository;
+import com.simon.exchange.services.NetSalaryCalculator;
 
 @RestController
 @RequestMapping("/api/exchange")
@@ -19,14 +23,22 @@ public class RequestController {
 
 	private final Logger logger = Logger.getLogger(RequestController.class);
 	
+	@Autowired
+	private NetSalaryCalculator calc;
+	
+	@Autowired
+	private CountryRepository repository;
+	
 	@RequestMapping(value="/countries",method=RequestMethod.GET)
 	public List<Country> getCountries(){
-		return null;
+		logger.info("Getting all countries");
+		return repository.findAll();
 	}
 	
 	@RequestMapping(value="/request", method = RequestMethod.POST)
-	public ClientResponse getClientResponse(ClientRequest request) {
-		return null;
+	public ClientResponse getClientResponse(@RequestBody ClientRequest request) {
+		logger.info("Input: " + request.getCountry() + " and " + request.getAmount());
+		return calc.calculate(request);
 	}
 	
 }
--- back/src/test/java/com/simon/exchange/controllers/RequestController_Test.java
+++ back/src/test/java/com/simon/exchange/controllers/RequestController_Test.java
@@ -4,33 +4,50 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.simon.exchange.domain.ClientRequest;
+import com.simon.exchange.domain.ClientResponse;
 import com.simon.exchange.domain.Country;
 import com.simon.exchange.services.CountryRepository;
+import com.simon.exchange.services.NetSalaryCalculator;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RequestController.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class RequestController_Test {
 	
 	@Autowired
     private MockMvc mvc;
 	
+	@Autowired
+	private RequestController controller;
+	
 	@MockBean
     private CountryRepository repository;
+	
+	@MockBean
+	private NetSalaryCalculator calc;
 
 	@Test
 	public void testGetCountries_shouldReturnEmptyList() throws Exception {
@@ -51,7 +68,7 @@ public class RequestController_Test {
 		List<Country> toTest = Arrays.asList(first,second);
 		given(repository.findAll()).willReturn(toTest);
 		
-		mvc.perform(get("/api/messages")
+		mvc.perform(get("/api/exchange/countries")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$").exists())
@@ -66,8 +83,64 @@ public class RequestController_Test {
 	
 
 	@Test
-	public void testGetClientResponse() {
-		fail("Not yet implemented");
+	public void testGetClientResponse_shouldReturnValidResponse() throws Exception {
+		
+		ClientRequest request = new ClientRequest();
+		request.setAmount(420.00);
+		request.setCountry("UK");
+		
+		ClientResponse response = new ClientResponse();
+		response.setRequest(request);
+		response.setRate(4.5d);
+		response.setEffectiveDate("2018-01-01");
+		
+		given(calc.calculate(request)).willReturn(response);
+		ClientResponse test = controller.getClientResponse(request);
+		
+		Assert.assertEquals(4.5d,test.getRate(),0);
+		Assert.assertEquals("2018-01-01",test.getEffectiveDate());
+	
+	}
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetClientResponse_badCountryShouldReturnError() throws Exception {
+		
+		ClientRequest request = new ClientRequest();
+		request.setAmount(420.00);
+		request.setCountry(null);
+		
+		ClientResponse response = new ClientResponse();
+		response.setRequest(request);
+		response.setRate(4.5d);
+		response.setEffectiveDate("2018-01-01");
+		
+		expectedEx.expect(IllegalArgumentException.class);
+		
+		given(calc.calculate(request)).willThrow(IllegalArgumentException.class);
+		ClientResponse test = controller.getClientResponse(request);
+
+	}
+	
+	@Test
+	public void testGetClientResponse_negativeAmountShouldReturnError() {
+		
+		ClientRequest request = new ClientRequest();
+		request.setAmount(-420.00);
+		request.setCountry("UK");
+		
+		ClientResponse response = new ClientResponse();
+		response.setRequest(request);
+		response.setRate(4.5d);
+		response.setEffectiveDate("2018-01-01");
+		
+		expectedEx.expect(IllegalArgumentException.class);
+		
+		given(calc.calculate(request)).willThrow(IllegalArgumentException.class);
+		ClientResponse test = controller.getClientResponse(request);
 	}
 
 }
