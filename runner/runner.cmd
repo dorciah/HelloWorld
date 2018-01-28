df14e45d64872262bc7fe2e1aa5d5277276ec48e
sphall <simonhall37@hotmail.com>
2018-01-28 19:30:28 +0000

    Updated poms and cmd to version 1.0

--- runner/runner.cmd
+++ runner/runner.cmd
@@ -1,3 +1,3 @@
 @echo off 
 color 0A
-java -cp "target/runner-0.1.0-jar-with-dependencies.jar" com.simon.runner.Runner
+java -cp "target/runner-1.0-jar-with-dependencies.jar" com.simon.runner.Runner
